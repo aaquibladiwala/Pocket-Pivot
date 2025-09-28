277c2f14813d272fbe530f2e0bae3818fc96600c
aaquibladiwala <aaquibladiwala@gmail.com>
2025-09-28 15:27:35 +0530

    Update pocket_pivot_screener_streamlit.py

--- pocket_pivot_screener_streamlit.py
+++ pocket_pivot_screener_streamlit.py
@@ -15,7 +15,7 @@ logging.basicConfig(filename='errors.log', level=logging.WARNING,
 @st.cache_data
 def fetch_nse_symbols(max_symbols=None):
     try:
-        df = pd.read_csv(r'Downloads\EQUITY_L.csv')
+        df = pd.read_csv('https://github.com/aaquibladiwala/Pocket-Pivot/blob/main/EQUITY_L.csv')
         symbols = df['SYMBOL'].tolist()
         if max_symbols:
             symbols = symbols[:max_symbols]
